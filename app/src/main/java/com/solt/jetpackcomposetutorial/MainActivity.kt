88c546d198bcd17090e711c9f2500a7c9674292e
Ukpeh Gabriel Donatus <gabrielukpehdev@gmail.com>
2025-01-02 15:05:04 +0100

    Started Learning Jetpack Compose

---
+++ app/src/main/java/com/solt/jetpackcomposetutorial/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.solt.jetpackcomposetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.solt.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+//Today we will be learning Jetpack Compose
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            JetpackComposeTutorialTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Gabriel",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTutorialTheme {
+        Greeting("Android")
+    }
+}
